f9872145d06a151a59312b8bebe33611a23c9072
niyaz <n.nagimullin@gradoservice.ru>
2022-02-05 15:25:11 +0300

    Первая фиксация

---
+++ Payment.WebApi/Controllers/PaymentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Payment.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        // GET: api/<PaymentController>
+        [HttpGet]
+        public string Get()
+        {
+            return "This Payment service";
+        }
+    }
+}
---
+++ Product.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Product.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: api/<ProductController>
+        [HttpGet]
+        public string Get()
+        {
+            return "This Product service";
+        }
+    }
+}
